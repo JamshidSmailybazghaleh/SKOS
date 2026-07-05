d0df33b9790be8bc82543d7d2b3ae0c2ccb0a5e5
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-05 12:32:15 +0330

    Create entity-extractor.ts
    BUILD-000036
    
    Initialize Knowledge Engine Foundation

---
+++ subsystems/knowledge-engine/src/entity-extractor.ts
@@ -0,0 +1,15 @@
+/**
+ * ==========================================================
+ * Entity Extractor
+ * ==========================================================
+ */
+
+export class EntityExtractor {
+
+    public extract(text: string): string[] {
+
+        return [];
+
+    }
+
+}
