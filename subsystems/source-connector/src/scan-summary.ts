799ada91ebdb4bba1aff77ba1f75a0ab3ea06c7b
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-06 22:24:25 +0330

    Create scan-summary.ts
    BUILD-000143
    
    feat(source-connector): implement Archive Scanner Foundation

---
+++ subsystems/source-connector/src/scan-summary.ts
@@ -0,0 +1,20 @@
+/**
+ * BUILD-000143
+ * Scan Summary
+ */
+
+export interface ScanSummary {
+
+    totalFiles: number;
+
+    pdfFiles: number;
+
+    wordFiles: number;
+
+    imageFiles: number;
+
+    textFiles: number;
+
+    unknownFiles: number;
+
+}
