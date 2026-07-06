682fa9d83cfcea060bfbca1e71ca98077cef393a
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-06 22:21:54 +0330

    Create file-record.ts
    BUILD-000143
    
    feat(source-connector): implement Archive Scanner Foundation

---
+++ subsystems/source-connector/src/file-record.ts
@@ -0,0 +1,18 @@
+/**
+ * BUILD-000143
+ * Archive File Record
+ */
+
+export interface FileRecord {
+
+    name: string;
+
+    path: string;
+
+    extension: string;
+
+    size: number;
+
+    lastModified: number;
+
+}
