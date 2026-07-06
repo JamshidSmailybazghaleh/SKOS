94a226c8eac5b4048457be42e41f2288e31a836a
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-07 01:59:02 +0330

    Create publication-stage.ts
    BUILD-000154
    
    feat(publication): implement Publication Workflow Foundation

---
+++ subsystems/publication/src/publication-stage.ts
@@ -0,0 +1,17 @@
+export enum PublicationStage {
+
+    IMPORTED = "IMPORTED",
+
+    ORGANIZED = "ORGANIZED",
+
+    REVIEW = "REVIEW",
+
+    APPROVED = "APPROVED",
+
+    SAMPLE_READY = "SAMPLE_READY",
+
+    QUEUED = "QUEUED",
+
+    PUBLISHED = "PUBLISHED"
+
+}
