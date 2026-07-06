c7facf6279a92ebcc3fe2028db0d40e7e066b607
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-06 22:34:11 +0330

    Create cloud-provider.ts
    BUILD-000145
    
    feat(source-connector): implement Cloud Connector Foundation

---
+++ subsystems/source-connector/src/cloud-provider.ts
@@ -0,0 +1,22 @@
+/**
+ * ==========================================================
+ * BUILD-000145
+ * Cloud Provider
+ * ==========================================================
+ */
+
+export enum CloudProvider {
+
+    GOOGLE_DRIVE = "Google Drive",
+
+    ONEDRIVE = "OneDrive",
+
+    DROPBOX = "Dropbox",
+
+    GITHUB = "GitHub",
+
+    NEXTCLOUD = "Nextcloud",
+
+    CUSTOM = "Custom"
+
+}
