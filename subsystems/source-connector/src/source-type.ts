7797d14abb06acbd985856224013dfb28acc13bd
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-06 22:50:33 +0330

    Create source-type.ts
    BUILD-000146
    
    feat(source-connector): implement Multi-Source Merge Engine Foundation

---
+++ subsystems/source-connector/src/source-type.ts
@@ -0,0 +1,16 @@
+/**
+ * ==========================================================
+ * BUILD-000146
+ * Source Type
+ * ==========================================================
+ */
+
+export enum SourceType {
+
+    SD_CARD = "SD_CARD",
+
+    INTERNAL_STORAGE = "INTERNAL_STORAGE",
+
+    CLOUD = "CLOUD"
+
+}
