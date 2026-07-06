e8108fa6cb227abcc76f3ff482855d8607bb4277
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-06 22:35:56 +0330

    Create cloud-file.ts
    BUILD-000145
    
    feat(source-connector): implement Cloud Connector Foundation

---
+++ subsystems/source-connector/cloud-file.ts
@@ -0,0 +1,20 @@
+/**
+ * ==========================================================
+ * BUILD-000145
+ * Cloud File
+ * ==========================================================
+ */
+
+export interface CloudFile {
+
+    id: string;
+
+    name: string;
+
+    path: string;
+
+    size: number;
+
+    modified: number;
+
+}
