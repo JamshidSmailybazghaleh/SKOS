21ff270bbe7ebafa1e96412fddd9eee9aed074a2
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-06 22:30:58 +0330

    Create storage-path.ts
    BUILD-000144
    
    feat(source-connector): implement Internal Storage Connector Foundation

---
+++ subsystems/source-connector/src/storage-path.ts
@@ -0,0 +1,18 @@
+/**
+ * ==========================================================
+ * BUILD-000144
+ * Internal Storage Path
+ * ==========================================================
+ */
+
+export interface StoragePath {
+
+    id: string;
+
+    name: string;
+
+    path: string;
+
+    enabled: boolean;
+
+}
