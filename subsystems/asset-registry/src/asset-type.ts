3990ff4ecf4df84b90cd47bf33bdb052f012e23b
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-07 16:16:10 +0330

    Create asset-type.ts
    BUILD-000158
    
    Implement Asset Registry Engine Foundation

---
+++ subsystems/asset-registry/src/asset-type.ts
@@ -0,0 +1,21 @@
+export enum AssetType {
+
+    Word = "word",
+
+    PDF = "pdf",
+
+    Cover = "cover",
+
+    Image = "image",
+
+    EPUB = "epub",
+
+    HTML = "html",
+
+    Audio = "audio",
+
+    Video = "video",
+
+    Other = "other"
+
+}
