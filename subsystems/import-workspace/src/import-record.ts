04fb9de0acdabc251aac8044024349482cba2f35
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-07 15:46:14 +0330

    Create import-record.ts
    BUILD-000155
    
    Implement Import Workspace Foundation

---
+++ subsystems/import-workspace/src/import-record.ts
@@ -0,0 +1,21 @@
+export interface ImportRecord {
+
+    id: string;
+
+    source: string;
+
+    path: string;
+
+    filename: string;
+
+    extension: string;
+
+    size: number;
+
+    createdAt: number;
+
+    modifiedAt: number;
+
+    status: string;
+
+}
