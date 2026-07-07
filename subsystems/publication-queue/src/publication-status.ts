0e1d87ecb527ef888d7cbf9013ac7eba10e4d846
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-07 17:48:45 +0330

    Create publication-status.ts
    BUILD-000161
    
    Implement Publication Queue Manager Foundation

---
+++ subsystems/publication-queue/src/publication-status.ts
@@ -0,0 +1,15 @@
+export enum PublicationStatus {
+
+    Draft = "draft",
+
+    WaitingApproval = "waiting-approval",
+
+    Approved = "approved",
+
+    Scheduled = "scheduled",
+
+    Published = "published",
+
+    Rejected = "rejected"
+
+}
