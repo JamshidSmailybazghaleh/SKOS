ceb7e19394c45f24e88a5f71952d4400587137eb
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-07 15:56:06 +0330

    Create scan-record.ts
    BUILD-000156
    
    Implement Safe Import Scanner Foundation

---
+++ subsystems/safe-import-scanner/src/scan-record.ts
@@ -0,0 +1,19 @@
+export interface ScanRecord {
+
+    id: string;
+
+    source: string;
+
+    path: string;
+
+    filename: string;
+
+    extension: string;
+
+    size: number;
+
+    createdAt: number;
+
+    modifiedAt: number;
+
+}
