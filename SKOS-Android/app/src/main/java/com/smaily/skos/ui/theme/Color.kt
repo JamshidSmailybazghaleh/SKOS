f48b9be64f76c12f2dd0d54d102bf2f74df00f20
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-12 11:34:08 +0330

    Create Color.kt

---
+++ SKOS-Android/app/src/main/java/com/smaily/skos/ui/theme/Color.kt
@@ -0,0 +1,41 @@
+/*
+ * ==========================================================
+ * SKOS
+ * Smaily Knowledge Operating System
+ * ==========================================================
+ *
+ * BUILD-000197
+ * File : Color.kt
+ * Version : 1.0.0
+ * ==========================================================
+ */
+
+package com.smaily.skos.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/*
+ * Official Brand Colors
+ */
+
+val MidnightBlue = Color(0xFF0A2342)
+
+val Gold = Color(0xFFD4AF37)
+
+val White = Color(0xFFFFFFFF)
+
+val Black = Color(0xFF000000)
+
+/*
+ * Supporting Colors
+ */
+
+val DarkBackground = Color(0xFF081A31)
+
+val SurfaceDark = Color(0xFF102A4C)
+
+val SuccessGreen = Color(0xFF2E7D32)
+
+val WarningOrange = Color(0xFFF9A825)
+
+val ErrorRed = Color(0xFFC62828)
