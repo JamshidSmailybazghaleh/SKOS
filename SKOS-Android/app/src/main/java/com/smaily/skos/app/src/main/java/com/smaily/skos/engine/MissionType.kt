f32d125eef48650b96cf5e11338e159fcf4f2fbb
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-12 23:31:48 +0330

    Create MissionType.kt

---
+++ SKOS-Android/app/src/main/java/com/smaily/skos/app/src/main/java/com/smaily/skos/engine/MissionType.kt
@@ -0,0 +1,27 @@
+package com.smaily.skos.engine
+
+enum class MissionType {
+
+    SCAN_FOLDER,
+
+    SCAN_INTERNAL_STORAGE,
+
+    SCAN_SD_CARD,
+
+    SCAN_CLOUD,
+
+    BUILD_SAMPLE,
+
+    BUILD_COMMERCIAL,
+
+    BUILD_METADATA,
+
+    PUBLISH_STORE,
+
+    PUBLISH_GITHUB,
+
+    BACKUP,
+
+    QUALITY_CHECK
+
+}
