3cf437db3618e2d3739e9519583b512b129ca9df
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-12 23:56:12 +0330

    Update ScannerEngine.kt

--- SKOS-Android/app/src/main/java/com/smaily/skos/scanner/ScannerEngine.kt
+++ SKOS-Android/app/src/main/java/com/smaily/skos/scanner/ScannerEngine.kt
@@ -1,69 +1,13 @@
 package com.smaily.skos.scanner
 
-import android.content.Context
-import androidx.documentfile.provider.DocumentFile
-import com.smaily.skos.storage.StorageManager
+import com.smaily.skos.engine.MissionResult
 
+interface ScannerEngine {
 
-/**
- * ==========================================================
- * SKOS
- * Smaily Knowledge Operating System
- * ==========================================================
- *
- * Module : Scanner Engine
- * Build  : BUILD-000201
- * Version: 1.0.0
- *
- * Responsibility:
- * اتصال StorageManager به FileScanner
- *
- * Flow:
- *
- * Storage URI
- *      ↓
- * DocumentFile
- *      ↓
- * FileScanner
- *      ↓
- * ScannedFile List
- *
- * ==========================================================
- */
+    fun scanFolder(
 
-class ScannerEngine(
-    private val context: Context,
-    private val storageManager: StorageManager
-) {
+        path: String
 
-
-    private val fileScanner =
-        FileScanner()
-
-
-
-    fun executeScan(): List<ScannedFile> {
-
-
-        val uri =
-            storageManager.getStorageUri()
-                ?: return emptyList()
-
-
-
-        val rootDirectory =
-            DocumentFile.fromTreeUri(
-                context,
-                uri
-            )
-                ?: return emptyList()
-
-
-
-        return fileScanner.scan(
-            rootDirectory
-        )
-
-    }
+    ): MissionResult
 
 }
