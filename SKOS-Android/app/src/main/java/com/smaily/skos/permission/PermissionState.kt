d0f1638804d37b20f5f0977e5fa4eabce755312b
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-12 12:21:22 +0330

    Create PermissionState.kt

---
+++ SKOS-Android/app/src/main/java/com/smaily/skos/permission/PermissionState.kt
@@ -0,0 +1,25 @@
+/*
+ * ==========================================================
+ * SKOS
+ * Smaily Knowledge Operating System
+ * ==========================================================
+ *
+ * BUILD-000199
+ * File : PermissionState.kt
+ * Version : 1.0.0
+ * ==========================================================
+ */
+
+package com.smaily.skos.permission
+
+enum class PermissionState {
+
+    UNKNOWN,
+
+    GRANTED,
+
+    DENIED,
+
+    PERMANENTLY_DENIED
+
+}
