c5eb5fb3ad10c4065d82b5ce42d423175e9b008b
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-13 09:51:30 +0330

    Update ScanStatistics.kt

--- SKOS-Android/app/src/main/java/com/smaily/skos/scanner/ScanStatistics.kt
+++ SKOS-Android/app/src/main/java/com/smaily/skos/scanner/ScanStatistics.kt
@@ -2,28 +2,30 @@ package com.smaily.skos.scanner
 
 data class ScanStatistics(
 
-    var folders:Int = 0,
+    var folders: Int = 0,
 
-    var files:Int = 0,
+    var files: Int = 0,
 
-    var pdf:Int = 0,
+    var books: Int = 0,
 
-    var epub:Int = 0,
+    var documents: Int = 0,
 
-    var docx:Int = 0,
+    var presentations: Int = 0,
 
-    var pptx:Int = 0,
+    var spreadsheets: Int = 0,
 
-    var xlsx:Int = 0,
+    var images: Int = 0,
 
-    var txt:Int = 0,
+    var audio: Int = 0,
 
-    var images:Int = 0,
+    var video: Int = 0,
 
-    var audio:Int = 0,
+    var archives: Int = 0,
 
-    var video:Int = 0,
+    var sourceCode: Int = 0,
 
-    var unknown:Int = 0
+    var databases: Int = 0,
+
+    var unknown: Int = 0
 
 )
