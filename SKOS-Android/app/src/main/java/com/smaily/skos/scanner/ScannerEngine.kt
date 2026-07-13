c711dc7ab98ee02ad5cb8dae8286a57eb5625ea2
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-13 09:27:57 +0330

    Update ScannerEngine.kt

--- SKOS-Android/app/src/main/java/com/smaily/skos/scanner/ScannerEngine.kt
+++ SKOS-Android/app/src/main/java/com/smaily/skos/scanner/ScannerEngine.kt
@@ -2,12 +2,38 @@ package com.smaily.skos.scanner
 
 import com.smaily.skos.engine.MissionResult
 
+/**
+ * موتور اصلی اسکن SKOS
+ */
 interface ScannerEngine {
 
+    /**
+     * شروع اسکن یک پوشه
+     */
     fun scanFolder(
 
         path: String
 
     ): MissionResult
+
+    /**
+     * توقف اسکن
+     */
+    fun stop()
+
+    /**
+     * توقف موقت
+     */
+    fun pause()
+
+    /**
+     * ادامه اسکن
+     */
+    fun resume()
+
+    /**
+     * آیا اسکن در حال اجرا است؟
+     */
+    fun isRunning(): Boolean
 
 }
