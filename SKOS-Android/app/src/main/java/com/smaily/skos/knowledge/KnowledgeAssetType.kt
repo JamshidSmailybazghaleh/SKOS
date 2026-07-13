403ef0a375c63c2775304fdf5babbfa3fef7c5ab
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-13 10:01:40 +0330

    Create KnowledgeAssetType.kt

---
+++ SKOS-Android/app/src/main/java/com/smaily/skos/knowledge/KnowledgeAssetType.kt
@@ -0,0 +1,32 @@
+package com.smaily.skos.knowledge
+
+/**
+ * انواع دارایی‌های دانشی در SKOS
+ */
+enum class KnowledgeAssetType {
+
+    BOOK,
+
+    ARTICLE,
+
+    DOCUMENT,
+
+    PRESENTATION,
+
+    SPREADSHEET,
+
+    IMAGE,
+
+    AUDIO,
+
+    VIDEO,
+
+    SOURCE_CODE,
+
+    DATABASE,
+
+    ARCHIVE,
+
+    UNKNOWN
+
+}
