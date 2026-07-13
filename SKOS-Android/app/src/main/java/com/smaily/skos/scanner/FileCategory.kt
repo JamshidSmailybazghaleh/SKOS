8113c408f8d99d14018f99dd5c9397d11f6a7ab0
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-13 09:39:44 +0330

    Create FileCategory.kt

---
+++ SKOS-Android/app/src/main/java/com/smaily/skos/scanner/FileCategory.kt
@@ -0,0 +1,27 @@
+package com.smaily.skos.scanner
+
+enum class FileCategory {
+
+    BOOK,
+
+    DOCUMENT,
+
+    PRESENTATION,
+
+    SPREADSHEET,
+
+    IMAGE,
+
+    AUDIO,
+
+    VIDEO,
+
+    ARCHIVE,
+
+    SOURCE_CODE,
+
+    DATABASE,
+
+    UNKNOWN
+
+}
