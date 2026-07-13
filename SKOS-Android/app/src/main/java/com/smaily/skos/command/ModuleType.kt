5cdab7b8539ec19ea03a38af78df694119389142
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-13 12:52:55 +0330

    Create ModuleType.kt

---
+++ SKOS-Android/app/src/main/java/com/smaily/skos/command/ModuleType.kt
@@ -0,0 +1,32 @@
+package com.smaily.skos.command
+
+/**
+ * انواع ماژول‌های اصلی SKOS
+ */
+enum class ModuleType {
+
+    ENGINE,
+
+    STORAGE,
+
+    SCANNER,
+
+    KNOWLEDGE,
+
+    RECOGNITION,
+
+    PUBLICATION,
+
+    DASHBOARD,
+
+    ANALYTICS,
+
+    SECURITY,
+
+    BACKUP,
+
+    NETWORK,
+
+    SETTINGS
+
+}
