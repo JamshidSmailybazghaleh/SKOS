0ecc67f069037e67c7c1438a878387cdad6581f9
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-15 18:53:04 +0330

    Create PipelineState.kt

---
+++ SKOS-Android/app/src/main/java/com/smaily/skos/pipeline/PipelineState.kt
@@ -0,0 +1,12 @@
+package com.smaily.skos.pipeline
+
+enum class PipelineState {
+    CREATED,
+    INITIALIZED,
+    VALIDATED,
+    RUNNING,
+    COMPLETED,
+    STOPPED,
+    FAILED,
+    DISPOSED
+}
