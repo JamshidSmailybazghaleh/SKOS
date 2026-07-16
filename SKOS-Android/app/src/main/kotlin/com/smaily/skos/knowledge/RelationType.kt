1034b42367dbffa056ea307ccbdd36cf788ced20
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-16 12:06:50 +0330

    Create RelationType.kt

---
+++ SKOS-Android/app/src/main/kotlin/com/smaily/skos/knowledge/RelationType.kt
@@ -0,0 +1,27 @@
+package com.smaily.skos.knowledge
+
+/**
+ * Standard relation types used in the SKOS Knowledge Graph.
+ */
+enum class RelationType {
+
+    REFERENCES,
+
+    CONTAINS,
+
+    BELONGS_TO,
+
+    DEPENDS_ON,
+
+    RELATED_TO,
+
+    WRITTEN_BY,
+
+    CREATED_BY,
+
+    LOCATED_IN,
+
+    PART_OF,
+
+    CITES
+}
