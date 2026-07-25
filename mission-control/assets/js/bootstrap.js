6327231bcc8a2cb342f0dd55f2d8642f4575c518
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-25 21:32:19 +0330

    Create bootstrap.js

---
+++ mission-control/assets/js/bootstrap.js
@@ -0,0 +1,85 @@
+/*
+====================================================
+SKOS Mission Control
+
+Bootstrap
+
+File:
+bootstrap.js
+
+Version:
+1.0
+
+Status:
+ACTIVE
+====================================================
+*/
+
+const Bootstrap = {
+
+    async initialize() {
+
+        console.log(
+            "================================"
+        );
+
+        console.log(
+            "Bootstrap Started"
+        );
+
+        console.log(
+            "================================"
+        );
+
+        try {
+
+            if (typeof CONFIG === "undefined") {
+
+                throw new Error(
+                    "CONFIG not loaded."
+                );
+
+            }
+
+            if (typeof SKOS === "undefined") {
+
+                throw new Error(
+                    "Kernel not loaded."
+                );
+
+            }
+
+            await SKOS.initialize();
+
+            console.log(
+                "Bootstrap Finished."
+            );
+
+        }
+
+        catch (error) {
+
+            console.error(
+                "Bootstrap Error:",
+                error
+            );
+
+        }
+
+    }
+
+};
+
+window.addEventListener(
+
+    "DOMContentLoaded",
+
+    () => {
+
+        Bootstrap.initialize();
+
+    }
+
+);
+
+Object.freeze(Bootstrap);
