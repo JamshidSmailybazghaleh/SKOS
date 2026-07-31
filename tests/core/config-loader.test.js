c6b9a11367dd15e8fc19bac8ead5c72a5a39d1cc
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-07-31 22:25:08 +0330

    Create config-loader.test.js

---
+++ tests/core/config-loader.test.js
@@ -0,0 +1,100 @@
+/**
+ * SKOS Config Loader Test
+ *
+ * TEST-CORE-002
+ */
+
+
+const ConfigLoader =
+require(
+"../../src/core/config-loader"
+);
+
+
+
+describe(
+"SKOS Config Loader Tests",
+()=>{
+
+
+let loader;
+
+
+
+beforeEach(()=>{
+
+
+loader =
+new ConfigLoader();
+
+
+
+loader.initialize();
+
+
+
+});
+
+
+
+test(
+"Loader should initialize",
+()=>{
+
+
+expect(
+loader.getStatus().status
+)
+.toBe(
+"INITIALIZED"
+);
+
+
+});
+
+
+
+test(
+"Configuration should load",
+()=>{
+
+
+const config =
+loader.load();
+
+
+
+expect(
+config.system
+)
+.toBe(
+"SKOS"
+);
+
+
+});
+
+
+
+test(
+"Configuration should validate",
+()=>{
+
+
+loader.load();
+
+
+
+expect(
+loader.validate()
+)
+.toBe(
+true
+);
+
+
+});
+
+
+
+});
