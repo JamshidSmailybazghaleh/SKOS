be5281d7117e3ca92c7e1a1d35433bc51b0a9a5b
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-08-01 02:27:20 +0330

    Create history-manager.test.js

---
+++ tests/engines/sdkc-engine/history-manager.test.js
@@ -0,0 +1,30 @@
+const HistoryManager =
+require(
+"../../src/engines/sdkc-engine/history-manager"
+);
+
+
+test(
+"History event should register",
+()=>{
+
+
+const result =
+history.addEvent(
+
+"SKOS-KO-000001",
+
+"TEST_EVENT"
+
+);
+
+
+expect(
+result.event
+)
+.toBe(
+"TEST_EVENT"
+);
+
+
+});
