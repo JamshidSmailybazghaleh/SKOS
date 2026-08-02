a21e29968002ee4ee07c1c22db78cd129a479946
JamshidSmailybazghaleh <iran.jsbsmaily1@gmail.com>
2026-08-02 12:05:38 +0330

    Update sdkc-engine.js

--- src/engines/sdkc-engine/sdkc-engine.js
+++ src/engines/sdkc-engine/sdkc-engine.js
@@ -2,118 +2,428 @@
  * SKOS SDKC Engine
  *
  * File ID: ENG-004
- * Build: BUILD-000001
- * Version: 1.0.0
+ * Build: BUILD-000004
+ * Version: 1.1.0
+ *
+ * Monitoring Hook Integration
  */
+
 
 class SDKCEngine {
 
+
     constructor(options = {}) {
 
-        this.logger = options.logger || null;
 
-        this.status = "CREATED";
+        this.logger =
+            options.logger || null;
 
-        this.repository = new Map();
+
+        this.monitoring =
+            options.monitoring || null;
+
+
+
+        this.status =
+            "CREATED";
+
+
+        this.repository =
+            new Map();
+
 
     }
+
+
+
+
 
     initialize() {
 
-        this.status = "INITIALIZED";
 
-        this.log("SDKC_ENGINE_INITIALIZED");
+        this.status =
+            "INITIALIZED";
+
+
+        this.log(
+            "SDKC_ENGINE_INITIALIZED"
+        );
+
+
+
+        this.recordMonitoringEvent(
+
+            "SDKC_ENGINE_INITIALIZED"
+
+        );
+
 
         return true;
 
+
     }
+
+
+
+
 
     store(object) {
 
-        if (!object || !object.id) {
 
-            throw new Error("Knowledge Object must contain a valid id.");
+        if (
+            !object ||
+            !object.id
+        ) {
+
+
+            throw new Error(
+
+                "Knowledge Object must contain a valid id."
+
+            );
+
 
         }
 
-        this.repository.set(object.id, object);
 
-        this.log("OBJECT_STORED", { id: object.id });
+
+        this.repository.set(
+
+            object.id,
+
+            object
+
+        );
+
+
+
+        this.log(
+
+            "OBJECT_STORED",
+
+            {
+
+                id:
+                    object.id
+
+            }
+
+        );
+
+
+
+        this.recordMonitoringEvent(
+
+            "SDKC_OBJECT_STORED",
+
+            {
+
+                objectId:
+                    object.id
+
+            }
+
+        );
+
+
+
+        this.updateMonitoringMetric(
+
+            "objectsStored"
+
+        );
+
+
 
         return object;
 
+
     }
+
+
+
+
 
     retrieve(id) {
 
-        return this.repository.get(id) || null;
+
+        const result =
+            this.repository.get(id)
+            || null;
+
+
+
+        if (result) {
+
+
+            this.recordMonitoringEvent(
+
+                "SDKC_OBJECT_RETRIEVED",
+
+                {
+
+                    objectId:
+                        id
+
+                }
+
+            );
+
+
+        }
+
+
+
+        return result;
+
 
     }
+
+
+
+
 
     exists(id) {
 
+
         return this.repository.has(id);
 
+
     }
+
+
+
+
 
     list() {
 
-        return Array.from(this.repository.values());
+
+        return Array.from(
+
+            this.repository.values()
+
+        );
+
 
     }
+
+
+
+
 
     count() {
 
+
         return this.repository.size;
 
+
     }
+
+
+
+
 
     remove(id) {
 
-        const deleted = this.repository.delete(id);
+
+        const deleted =
+            this.repository.delete(id);
+
+
 
         if (deleted) {
 
-            this.log("OBJECT_REMOVED", { id });
+
+            this.log(
+
+                "OBJECT_REMOVED",
+
+                {
+
+                    id
+
+                }
+
+            );
+
+
+
+            this.recordMonitoringEvent(
+
+                "SDKC_OBJECT_REMOVED",
+
+                {
+
+                    objectId:
+                        id
+
+                }
+
+            );
+
 
         }
+
+
 
         return deleted;
 
+
     }
+
+
+
+
 
     getStatus() {
 
+
         return {
 
-            status: this.status,
 
-            totalObjects: this.count()
+            status:
+                this.status,
+
+
+            totalObjects:
+                this.count()
+
 
         };
 
+
     }
 
-    log(message, metadata = {}) {
 
-        if (this.logger) {
 
-            this.logger.info(message, metadata);
+
+
+    recordMonitoringEvent(
+
+        event,
+
+        details = {}
+
+    ) {
+
+
+
+        if (
+            this.monitoring &&
+            this.monitoring.recordEvent
+        ) {
+
+
+            this.monitoring.recordEvent(
+
+                event,
+
+                details
+
+            );
+
 
         }
 
+
     }
+
+
+
+
+
+    updateMonitoringMetric(
+
+        metric
+
+    ) {
+
+
+        if (
+            this.monitoring &&
+            this.monitoring.updateMetric
+        ) {
+
+
+            this.monitoring.updateMetric(
+
+                metric
+
+            );
+
+
+        }
+
+
+    }
+
+
+
+
+
+    log(
+
+        message,
+
+        metadata = {}
+
+    ) {
+
+
+        if (this.logger) {
+
+
+            this.logger.info(
+
+                message,
+
+                metadata
+
+            );
+
+
+        }
+
+
+    }
+
+
+
+
 
     shutdown() {
 
-        this.status = "SHUTDOWN";
 
-        this.log("SDKC_ENGINE_SHUTDOWN");
+        this.status =
+            "SHUTDOWN";
+
+
+
+        this.log(
+
+            "SDKC_ENGINE_SHUTDOWN"
+
+        );
+
+
+
+        this.recordMonitoringEvent(
+
+            "SDKC_ENGINE_SHUTDOWN"
+
+        );
+
+
 
         return true;
 
+
     }
+
 
 }
 
-module.exports = SDKCEngine;
+
+
+module.exports =
+    SDKCEngine;
